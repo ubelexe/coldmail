3f45c4042356f28305c63e8595bd63e52ffac4f9
Andrey Ledyaykin <ubelexe@yandex.ru>
2018-07-03 16:00:13 +0300

    Change migration for index to letter

--- db/migrate/20180703113316_create_letters.rb
+++ db/migrate/20180703113316_create_letters.rb
@@ -2,11 +2,10 @@ class CreateLetters < ActiveRecord::Migration[5.2]
   def change
     create_table :letters do |t|
       t.string :url_site
-      t.string :email
+      t.string :email, index: true
       t.text :comment
 
       t.timestamps
     end
-    add_index :letters, :email
   end
 end
