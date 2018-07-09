97617355d11b00d9cf2cc07cf0c82c678f101505
Andrey Ledyaykin <ubelexe@yandex.ru>
2018-07-09 16:21:18 +0300

    AASM

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,5 +1,23 @@
 class Letter < ApplicationRecord
+  include AASM
   validates :url_site, :email, presence: true
 
   scope :search_by_fields, ->(q_string) { where("email LIKE :q OR url_site LIKE :q", q: "%#{q_string}%") }
+
+  aasm do
+    state :new, :initial => true
+    state :running, :sleeping, :completed
+
+    event :run do
+      transitions :from => [:new, :sleeping], :to => :running
+    end
+
+    event :done do
+      transitions :from => :running, :to => :completed
+    end
+
+    event :sleep do
+      transitions :from => :running, :to => :sleeping
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_113316) do
+ActiveRecord::Schema.define(version: 2018_07_09_123331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_07_03_113316) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "aasm_state"
     t.index ["email"], name: "index_letters_on_email"
   end
 
