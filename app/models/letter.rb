55c82d8d7b8fe9f0425d648b55015e6237930154
Andrey Ledyaykin <ubelexe@yandex.ru>
2018-07-09 16:57:22 +0300

    search include letter.rb

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,3 +1,5 @@
 class Letter < ApplicationRecord
   validates :url_site, :email, presence: true
+
+  scope :search_by_fields, ->(q_string) { where("email LIKE :q OR url_site LIKE :q", q: "%#{q_string}%") }
 end
