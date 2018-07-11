5f144a875f80eb19afb2666a4ff99d15203671fb
Andrey Ledyaykin <ubelexe@yandex.ru>
2018-07-11 13:13:42 +0300

    AASM6

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -5,19 +5,19 @@ class Letter < ApplicationRecord
   scope :search_by_fields, ->(q_string) { where("email LIKE :q OR url_site LIKE :q", q: "%#{q_string}%") }
 
   aasm do
-    state :new, :initial => true
+    state :new, initial: true
     state :running, :sleeping, :completed
 
     event :run do
-      transitions :from => [:new, :sleeping], :to => :running
+      transitions from: [:new, :sleeping], to: :running
     end
 
     event :done do
-      transitions :from => :running, :to => :completed
+      transitions from: :running, to: :completed
     end
 
     event :sleep do
-      transitions :from => :running, :to => :sleeping
+      transitions from: :running, to: :sleeping
     end
   end
 end
